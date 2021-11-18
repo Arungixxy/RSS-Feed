d0e0955e6563d953def0f139cbef814f98d9f4d8
Arungixxy <57450415+Arungixxy@users.noreply.github.com>
2021-11-18 18:43:38 +0530

    Initail commit

---
+++ packages/core/src/index.ts
@@ -0,0 +1,41 @@
+import {
+  Article,
+  ArticleContext,
+  ArticleRule,
+  ContentResolutionType,
+  ContentType,
+  FeedParser,
+  FeedParserOptions,
+  FeedParserResult,
+  FeedUrl,
+  LinkGroup,
+  LinkPointer,
+  Logger,
+  OutputType,
+  SimpleFeedResult,
+  SourceType,
+  TitleFeatures
+} from './feed-parser';
+
+import {LogCollector} from './log-collector';
+
+export {
+  FeedParser,
+  Article,
+  Logger,
+  SimpleFeedResult,
+  FeedUrl,
+  ContentResolutionType,
+  SourceType,
+  FeedParserResult,
+  FeedParserOptions,
+  LinkPointer,
+  ContentType,
+  LogCollector,
+  ArticleRule,
+  TitleFeatures,
+  OutputType,
+  ArticleContext,
+  LinkGroup
+};
+
